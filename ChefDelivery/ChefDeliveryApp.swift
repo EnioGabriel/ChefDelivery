faacfcbf187fe766aad06d7aac8182c5d979e7b6
Enio Gabriel <gabrielferreira.sc@gmail.com>
2023-08-22 20:42:32 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Enio  Gabriel Ferreira Silva on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
