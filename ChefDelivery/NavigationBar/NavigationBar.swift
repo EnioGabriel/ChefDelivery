2e1f42d926f283a0acef38b59886bb04ad4e5472
Enio Gabriel <gabrielferreira.sc@gmail.com>
2023-08-22 21:30:41 -0300

    feat: Criando escopo da NavigationBar

---
+++ ChefDelivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Enio  Gabriel Ferreira Silva on 22/08/23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack {
+            Button("R.Vergueiro, 3185") {
+                
+            }
+            
+            Button("Notificações") {
+                
+            }
+        }
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+    }
+}
